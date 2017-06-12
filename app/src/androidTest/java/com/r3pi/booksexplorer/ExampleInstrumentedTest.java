5eddcd5b937b8779c6afb332aad25963cf6e7e2a
brain <brain@brains-MacBook-Pro.local>
2017-06-12 23:37:57 +0200

    add espresso test

--- app/src/androidTest/java/com/r3pi/booksexplorer/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/r3pi/booksexplorer/ExampleInstrumentedTest.java
@@ -2,11 +2,34 @@ package com.r3pi.booksexplorer;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.NoMatchingViewException;
+import android.support.test.espresso.ViewAssertion;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.v7.widget.RecyclerView;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
 
+import com.r3pi.booksexplorer.ui.BooksList.BooksListActivity;
+
+import org.hamcrest.Matcher;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.pressKey;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 /**
@@ -16,6 +39,14 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    private static final String SEARCH_STRING = "lord of the rings";
+    private static final int NUM_ITEMS_FIRST_REQUEST = 10;
+
+    @Rule
+    public ActivityTestRule<BooksListActivity> mActivityRule = new ActivityTestRule<>(BooksListActivity.class);
+
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
@@ -23,4 +54,45 @@ public class ExampleInstrumentedTest {
 
         assertEquals("com.r3pi.booksexplorer", appContext.getPackageName());
     }
+
+    @Test
+    public void testSearchBook() throws Exception {
+        onView(isAssignableFrom(EditText.class)).perform(typeText(SEARCH_STRING), pressKey(KeyEvent.KEYCODE_ENTER));
+
+        onView(withId(R.id.list)).check(new RecyclerViewItemCountAssertion(NUM_ITEMS_FIRST_REQUEST));
+    }
+
+    @Test
+    public void testScrollAndFetchMore() throws Exception {
+        testSearchBook();
+
+        //onView(withId(R.id.list)).perform(RecyclerViewActions.actionOnItemAtPosition(3, click()));
+
+    }
+
+    private class RecyclerViewItemCountAssertion implements ViewAssertion {
+
+        private final Matcher<Integer> matcher;
+
+        public RecyclerViewItemCountAssertion(int expectedCount) {
+            this.matcher = is(expectedCount);
+        }
+
+        public RecyclerViewItemCountAssertion(Matcher<Integer> matcher) {
+            this.matcher = matcher;
+        }
+
+        @Override
+        public void check(View view, NoMatchingViewException noViewFoundException) {
+            if (noViewFoundException != null) {
+                throw noViewFoundException;
+            }
+
+            RecyclerView recyclerView = (RecyclerView) view;
+            RecyclerView.Adapter adapter = recyclerView.getAdapter();
+            assertThat(adapter.getItemCount(), matcher);
+        }
+
+    }
+
 }
